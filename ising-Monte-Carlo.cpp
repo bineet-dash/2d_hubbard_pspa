764fb7589349f5b05a7a358cf05aad44a54fd831
Bineet large-PC <bineet.dash@niser.ac.in>
2019-03-09 15:53:16 +0530

    compatible functions for PSPA

--- ising-Monte-Carlo.cpp
+++ ising-Monte-Carlo.cpp
@@ -3,117 +3,112 @@
 
 int main(int argc, char* argv[])
 {
-  if(argc!=4) {cerr << "Enter (1) lattice size, (2) U and (3) no of sweeps.\n"; exit(1);}
+ if(argc!=4) {cerr << "Enter (1) lattice size, (2) U and (3) no of sweeps.\n"; exit(1);}
   size = atoi(argv[1]);
   U = atof(argv[2]);
   int no_sweeps = atoi(argv[3]);
   int N_therm = 0.5*no_sweeps;
   int N_meas = no_sweeps-N_therm;
 
-  int initial_exp = -3;
-  int final_exp = 0;
-  double final_temp = 10*pow(10,final_exp);
-
-  milliseconds begin_ms = duration_cast< milliseconds >(system_clock::now().time_since_epoch());
+  int initial_exp = -2;
+  int final_exp = -1;
+  milliseconds begin_ms, end_ms;
+  long idum = time(NULL);
 
   MatrixXd randsigma=MatrixXd::Zero(size*size,3);
-  long idum = time(NULL);
-  for(int i=0; i<randsigma.rows(); i++) randsigma(i,2) = pow(1,xc(i)+yc(i)); //randsigma(i,2) = 5;
-
+  randsigma.col(2) = VectorXd::Constant(size*size,1);
+  for(int i=0; i<randsigma.rows(); i++) ising_sigma_generate(randsigma, i,idum);// randsigma(i,2) = pow(1,xc(i)+yc(i)); //randsigma(i,2) = 5;
+  MatrixXd suggested_randsigma = randsigma;
   MatrixXcd H0 = construct_h0_2d(); 
   MatrixXcd Id = MatrixXcd::Identity(H0.rows(),H0.cols());
 
-  MatrixXcd initial_Hamiltonian = H0-U/2*matrixelement_sigmaz_2d(randsigma);//+U/4*randsigma.rows()*Id;
-  // cout << initial_Hamiltonian << endl << endl << endl;
-  double free_energy = spa_free_energy(initial_Hamiltonian, final_temp);
+  ofstream outfile_mlength, outfile_data;
+  string dir_path;
 
-  MatrixXd suggested_randsigma = randsigma;
+  string latticedata = "U_"+to_string(int(U))+"_size_"+to_string(size)+"_sweeps_"+to_string(no_sweeps)+"_t_"+current_time_str();
+  dir_path = "spa_mc/"+latticedata+"/";
+  string create_dir_command =  "mkdir -p "+dir_path;
+  const int create_dir_result = system(create_dir_command.c_str()); 
+  if(create_dir_result==-1){cerr << "directory creation failed!\n"; exit(1);}
 
-  string filename, latticedata;
-  latticedata = "_U_"+to_string(int(U))+"_size_"+to_string(size)+"_sweeps_"+to_string(no_sweeps);
-  filename="spa_mc/spin_arrangement"+current_time_str()+latticedata+".nb"; ofstream outfile_spinarr(filename);
-  spinarrangement_2d_Mathematica_output(randsigma,outfile_spinarr);
-  filename="spa_mc/results_ising_spa_"+current_time_str()+latticedata+".txt"; ofstream outfile_data(filename);
+  string filename=dir_path+"data.dat";  outfile_data.open(filename);
   cout << "==============================\n"<< "filename is: " << filename << "\n========================\n";
+  
 
-  ofstream debugout("debug.txt");
+  double final_temp = 9*pow(10,final_exp);
+  MatrixXcd H_spa = H0-U/2*matrixelement_sigmaz_2d(randsigma)+U/4*randsigma.rows()*Id;
+  pair<MatrixXcd,VectorXd> spa_spectrum = Eigenspectrum(H_spa);
+  double spa_F = spa_free_energy(spa_spectrum.second, final_temp);
 
-  //for(int j=final_exp; j>=initial_exp; j--)
-  //{
-     double decrement = 1;
-    // decrement = (j==-1)?0.05:1;
-    //for(double i=9; i>=1; i-= decrement)
-    //{
-      //double temperature = i*pow(10,j);
-     for(double temperature = 6; temperature >= 0.01; temperature -= decrement)
-     {
- 	    decrement = (temperature < 0.3)?0.01:((temperature<2)?0.2:1); 
-      for(int sweep=0; sweep< N_therm; sweep++)
+  vector <double> T_range {0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.08,0.06,0.04,0.02,0.01};
+  for(double temperature : T_range)
+  {
+    for(int sweep=0; sweep< N_therm; sweep++)
+    {
+      for(int lattice_index=0; lattice_index<size*size; lattice_index++)
       {
-        for(int lattice_index=0; lattice_index<size*size; lattice_index++)
+        ising_sigma_generate(suggested_randsigma, lattice_index, idum);
+        MatrixXcd suggested_Hspa = H0-U/2*matrixelement_sigmaz_2d(suggested_randsigma);//+ U/4*randsigma.rows()*Id;
+        pair<MatrixXcd,VectorXd> suggested_spa_spectrum = Eigenspectrum(suggested_Hspa);
+        double suggested_spa_F = spa_free_energy(suggested_spa_spectrum.second, temperature);
+
+        double uniform_rv = ran0(&idum); 
+        double move_prob = exp((spa_F - suggested_spa_F)/temperature);
+
+        if(uniform_rv <= move_prob)
         {
-          ising_sigma_generate(suggested_randsigma, lattice_index, idum);
-          MatrixXcd suggested_Hamiltonian = H0-U/2*matrixelement_sigmaz_2d(suggested_randsigma);//+ U/4*randsigma.rows()*Id;
-          double suggested_free_energy = spa_free_energy(suggested_Hamiltonian, temperature);
-
-          // debugout << suggested_free_energy << " " << free_energy << endl;
-
-          double uniform_rv = ran0(&idum); double move_prob = exp((free_energy - suggested_free_energy)/temperature);
-
-          if(uniform_rv <= move_prob)
-          {
-            free_energy = suggested_free_energy;
-            randsigma = suggested_randsigma;
-          }
-          else
-          {
-            suggested_randsigma=randsigma;
-          }
+          spa_F = suggested_spa_F;
+          randsigma = suggested_randsigma;
+        }
+        else
+        {
+          suggested_randsigma=randsigma;
         }
       }
-
-      double final_free_energy = 0; double S_pi =0.0; double magnetisation=0.0; 
-      for(int sweep= N_therm; sweep<no_sweeps; sweep++)
-      {
-        for(int lattice_index=0; lattice_index<size*size; lattice_index++)
-        {
-          ising_sigma_generate(suggested_randsigma, lattice_index, idum);
-          MatrixXcd suggested_Hamiltonian = H0-U/2* matrixelement_sigmaz_2d(suggested_randsigma);//+U/4*randsigma.rows()*Id;
-          double suggested_free_energy = spa_free_energy(suggested_Hamiltonian, temperature);
-          // debugout << suggested_free_energy << " " << free_energy << endl;
-
-          double uniform_rv = ran0(&idum); double move_prob = exp((free_energy - suggested_free_energy)/temperature);
-
-          if(uniform_rv <= move_prob)
-          {
-            free_energy = suggested_free_energy;
-            randsigma = suggested_randsigma;
-          }
-          else
-          {
-            suggested_randsigma=randsigma;
-          }
-        }
-        final_free_energy += free_energy/(size*size); 
-        magnetisation += randsigma.col(2).sum()/(size*size);
-        S_pi += get_spi(randsigma);
-        
-        cout << "\r sweep = " << sweep << " done."; cout.flush();
-      }
-
-      outfile_data << temperature << " " << final_free_energy/N_meas << " " << S_pi/N_meas << " " << magnetisation/N_meas << endl;
-      cout << "\rtemperature = " << temperature << " done."; cout.flush();
     }
-  //}
 
+    string fields_filename = dir_path+"field_at_T_"+to_string(temperature)+".dat";
+    outfile_mlength.open(fields_filename);
+    if(outfile_mlength.bad()){cerr << "fail to create file to store aux-fields!"; exit(13);}
+    double final_free_energy = 0; 
+    double S_pi =0.0;
+
+    for(int sweep= N_therm; sweep<no_sweeps; sweep++)
+    {
+      for(int lattice_index=0; lattice_index<size*size; lattice_index++)
+      {
+        ising_sigma_generate(suggested_randsigma, lattice_index, idum);
+        MatrixXcd suggested_Hspa = H0-U/2*matrixelement_sigmaz_2d(suggested_randsigma);//+ U/4*randsigma.rows()*Id;
+        pair<MatrixXcd,VectorXd> suggested_spa_spectrum = Eigenspectrum(suggested_Hspa);
+        double suggested_spa_F = spa_free_energy(suggested_spa_spectrum.second, temperature);
+
+        double uniform_rv = ran0(&idum); 
+        double move_prob = exp((spa_F - suggested_spa_F)/temperature);
+
+        if(uniform_rv <= move_prob)
+        {
+          spa_F = suggested_spa_F;
+          randsigma = suggested_randsigma;
+        }
+        else
+        {
+          suggested_randsigma=randsigma;
+        }
+      }
+      final_free_energy += spa_F/(size*size); 
+      S_pi += get_spi(randsigma);
+      outfile_mlength << sweep <<  " " << randsigma.col(2).transpose() << endl;
+      
+      cout << "\r sweep = " << sweep << " done."; cout.flush();
+    }
+
+    outfile_data << temperature << " " << final_free_energy/N_meas << " " << S_pi/N_meas << " " << endl;
+    cout << "\rtemperature = " << temperature << " done."; cout.flush();
+    outfile_mlength.close();
+  }
+ 
   cout << endl;
-  milliseconds end_ms = duration_cast< milliseconds >(system_clock::now().time_since_epoch());
-  show_time(begin_ms, end_ms,"MC calculation");
-  spinarrangement_2d_Mathematica_output(randsigma,outfile_spinarr);
-
-  // outfile_mcdetails.close();
   outfile_data.close();
-  outfile_spinarr.close();
   return 0;
 }
 
@@ -130,3 +125,10 @@ for(int ix=0; ix<size; ix++)
 sort(debug.begin(),debug.end());  
 for(auto it=debug.begin(); it!=debug.end(); it++) cout << filter(*it) << " "; cout << endl;
 cout << endl; exit(1); */
+
+//for(int j=final_exp; j>=initial_exp; j--)
+//{
+  //for(double i=9; i>=1; i-= decrement)
+  //{
+
+  //}
